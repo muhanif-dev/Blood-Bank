a95d180225c4bce90ebfbdfb582b5cb4b8f53e22
muhanif-dev <muhanif.dev@gmail.com>
2026-09-22 23:11:57 +0500

    Fix donor detail API route

---
+++ api/donors/[id].js
@@ -0,0 +1,7 @@
+const connectDB = require("../../server/config/db");
+const app = require("../../server/server");
+
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
